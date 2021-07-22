d09def7f04539b6600e3b00578ef36deb3722159
AnKItdo <dankit264@gmail.com>
2021-07-22 13:24:24 +0545

    fake secret key

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '5gc&+kv&n_1!w1s=!7b6m-9b1^eu7640gqqa-cm8-xk9&z0^s%'
+SECRET_KEY = 'This is a fake secret key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
